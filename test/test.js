bdf862899fab4100f01f7fc909e9465a305307be
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-09-27 13:50:03 -1000

    initial setup

---
+++ test/test.js
@@ -0,0 +1,7 @@
+const chai = require("chai");
+const expect = chai.expect; // short handing "expect" instead of chai.expect
+const should = chai.should(); // short handing "should();"
+//const StringBuilder = require("../string-builder");
+
+
+describe();
