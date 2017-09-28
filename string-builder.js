5cd8d2e65cb9c84bee5f9be70873f2dc59791190
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-09-27 14:46:16 -1000

    second test passed, third test added

--- string-builder.js
+++ string-builder.js
@@ -1,7 +1,12 @@
 // jshint esversion:6
 
 class StringBuilder {
-
+  constructor(string){
+    this.string = string;
+}
+  normalString(){
+    return this.string.trim();
+}
 }
 
 module.exports = StringBuilder;
--- test/test.js
+++ test/test.js
@@ -7,14 +7,24 @@ const StringBuilder = require("../string-builder");
 
 
 describe('StringBuilder', function () {
+  let sb;
+
+  before(function (){
+    sb = new StringBuilder('test');
+  });
+
 
   it('should be a function', function () {
     StringBuilder.should.be.a('function');
-     });
+  });
 
   it('should be a string', function () {
     let sb = new StringBuilder('  test  ');
-    sb.string.should.equal('test');
+    sb.normalString().should.equal('test');
   });
-});
 
+  it("should return reversed string", function () {
+    sb.reverse().should.equal("tset");
+  });
+
+});
