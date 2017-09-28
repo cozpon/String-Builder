c5bd535ebb65322c1352f74fab9cc5f8b2507169
Nathan N <Nitharios@users.noreply.github.com>
2017-09-27 15:03:15 -1000

    Separated tests by function category

--- test/test.js
+++ test/test.js
@@ -5,34 +5,41 @@ const expect = chai.expect; // short handing "expect" instead of chai.expect
 const should = chai.should(); // short handing "should();"
 const StringBuilder = require("../string-builder");
 
+let sb;
+
+before(function (){
+  sb = new StringBuilder('test');
+});
 
 describe('StringBuilder', function () {
-  let sb;
-
-  before(function (){
-    sb = new StringBuilder('test');
-  });
-
 
   it('should be a function', function () {
     StringBuilder.should.be.a('function');
   });
+});
 
+describe('normalString', function() {
   it('should be a string', function () {
     let sb = new StringBuilder('  test  ');
     sb.normalString().should.equal('test');
   });
+});
 
+describe('reverse', function() {
   it("should return reversed string", function () {
     sb.reverse().should.equal("tset");
-  });
+  }); 
+});
 
+describe('capitalize', function() {
   it("should capitalize first letter of every word", function() {
     sb.capitalize().should.equal("Test");
   });
+});
 
+describe('toArray', function() {
   it("should turn String into an array", function() {
     sb.toArray().should.equal(["test"]);
   });
-
 });
+
